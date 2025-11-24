1656e742ae4df930079b9ef6f7d0dd83c030b8da
dhreya07 <pagaldrea@gmail.com>
2025-11-24 13:47:37 +0800

    Login TRy

--- src/views/GoogleForm.vue
+++ src/views/GoogleForm.vue
@@ -8,9 +8,9 @@
         <router-link to="/about" class="hover:text-red-600">About</router-link>
         <router-link to="/contact" class="hover:text-red-600">Contact</router-link>
         <router-link to="/google-form" class="text-red-600 border-b-2 border-red-600">Feedback</router-link>
-        <!-- <button @click="$router.push({ name: 'login' })" class="px-5 py-2 bg-red-600 text-white rounded-lg hover:bg-red-700 transition font-medium">
+        <button @click="$router.push({ name: 'login' })" class="px-5 py-2 bg-red-600 text-white rounded-lg hover:bg-red-700 transition font-medium">
           Login
-        </button> -->
+        </button>
       </nav>
     </header>
   <div class="feedback-wrapper">
